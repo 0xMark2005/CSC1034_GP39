48e5516526f9081151c2802c8ff40e2f50b5f328
d-onnelly <120676569+d-onnelly@users.noreply.github.com>
2025-02-28 05:05:46 +0000

    Refactor database connection script in test_connect.php for improved clarity and error handling

--- includes/test_connect.php
+++ includes/test_connect.php
@@ -1,2 +1,28 @@
-<?php echo (new mysqli("localhost","jdonnelly73","CHZHy02qM20fcLVt","CSC1034_CW_39"))->connect_error ? "Failed" : "Connected"; ?>
-// Output: Failed -no permissions so we wait for the proper connector to be posted
+<?php
+	//Sample Database Connection Syntax for PHP and MySQL.
+	
+	//Connect To Database
+	
+	$hostname="localhost";
+	$username="jdonnelly73";
+	$password="CHZHy02qM20fcLVt";
+	$dbname="CSC1034_CW_39";
+	$usertable="tools";
+	$yourfield = "itemName";
+	
+	mysqli_connect($hostname,$username, $password) or die ("<html><script language='JavaScript'>alert('Unable to connect to database! Please try again later.'),history.go(-1)</script></html>");
+	mysqli_select_db($dbname);
+	
+	# Check If Record Exists
+	
+	$query = "SELECT * FROM $usertable";
+	
+	$result = mysqli_query($query);
+	
+	if($result){
+		while($row = mysqli_fetch_array($result)){
+			$name = $row["$yourfield"];
+			echo "Name: ".$name."<br/>";
+		}
+	}
+?>
