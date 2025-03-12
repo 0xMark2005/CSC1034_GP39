2c1a15b2861a31b5cf8b09aa60420cdaaf2e3b7f
shea-h <sheahannaway@gmail.com>
2025-03-12 11:44:28 +0000

    Pushing for Joseph

--- js/game_js/game_tracker.js
+++ js/game_js/game_tracker.js
@@ -1,5 +1,6 @@
 export class GameTracker{
-    static nextAreaFilepath = "./game_story/burning-village";
+    static nextAreaFilepath = `./game_story/burning-village`;
+    static areaName = "bruning-village";
     static currentArea;
     static currentDialogue = "start";
 
