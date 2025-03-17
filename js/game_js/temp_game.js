91df314d093501518328a62f3d6d19000e04e8c1
shea-h <sheahannaway@gmail.com>
2025-03-17 17:57:33 +0000

    forgot to take it out of try catch

--- js/game_js/temp_game.js
+++ js/game_js/temp_game.js
@@ -92,84 +92,83 @@ function loadDialogue(){
     //output the message in the dialogue
     Terminal.outputMessage(currentAreaDialogue.message, dialogueColor);
 
+    //
+    // ADDING AND OUTPUTTING OPTIONS
+    //
+
     //loop through options and display
-    try{
-        //clear last set of options
-        let optionsLength = options.length;
-        if(options.length > 0){
-            for(let i=0; i < optionsLength; i++){
-                options.pop();
-            }
+    //clear last set of options
+    let optionsLength = options.length;
+    if(options.length > 0){
+        for(let i=0; i < optionsLength; i++){
+            options.pop();
         }
-
-        optionType = options.optionType; //set option type
-        let unfilteredOptions = currentAreaDialogue.options; //array to store all options, before removing ones that require logs
-        
-        //set all available options based on logs
-        for(let i=0; i < unfilteredOptions.length; i++){
-            let currentOption = unfilteredOptions[i]; //the current option from unfiltered list
-
-            if("unlockLog" in currentOption){ //if this option has an unlock log
-                for(let j=0; j<GameTracker.gameLogs.length; j++){
-
-                    let currentLog = GameTracker.gameLogs[j]; //the current log to be checked
-
-                    if(currentOption.unlockLog === currentLog){ //if the unlock log has been logged
-                        options.push(currentOption); //adds the current option to the list
-                        break;
-                    }
-                }
-            }
-            else if("lockLog" in currentOption){ //if this option has a lock log
-                options.push(currentOption); //adds the current option to the list (assumes lock log not met)
-
-                for(let j=0; j<GameTracker.gameLogs.length; j++){
-
-                    let currentLog = GameTracker.gameLogs[j]; //the current log to be checked
-
-                    if(currentOption.lockLog === currentLog){ //if the lock log has been logged
-                        options.pop(currentOption); //removes the current option from the list
-                        break;
-                    }
-                }
-            }
-            else{ //if no log condition
-                options.push(currentOption); //adds the current option to the list
-            }
-        }
-
-        //all options have now been added to options array
-        //display all available options
-        let outputString = ``;
-        if(optionType === "number"){
-            outputString = `Enter a number: \n`;
-            for(let i=0; i < options.length; i++){
-                let currentOption = options[i];
-                outputString += `${i+1}. ${currentOption.choice}`;
-
-                if(i !== options.length - 1){
-                    outputString += `\n`;
-                }
-            }
-            outputString += `${i+1}. `;
-        }
-        else{
-            outputString = `Enter a verb: `
-            for(let i=0; i < options.length; i++){
-                let currentOption = options[i];
-                outputString += `${currentOption.choice}`;
-
-                if(i !== options.length - 1){
-                    outputString += `, `;
-                }
-            }
-        }
-
-        Terminal.outputMessage(outputString, optionsColor);
     }
-    catch(error){
-        console.error("Error: ", error);
+
+    optionType = options.optionType; //set option type
+    let unfilteredOptions = currentAreaDialogue.options; //array to store all options, before removing ones that require logs
+    
+    //set all available options based on logs
+    for(let i=0; i < unfilteredOptions.length; i++){
+        let currentOption = unfilteredOptions[i]; //the current option from unfiltered list
+
+        if("unlockLog" in currentOption){ //if this option has an unlock log
+            for(let j=0; j<GameTracker.gameLogs.length; j++){
+
+                let currentLog = GameTracker.gameLogs[j]; //the current log to be checked
+
+                if(currentOption.unlockLog === currentLog){ //if the unlock log has been logged
+                    options.push(currentOption); //adds the current option to the list
+                    break;
+                }
+            }
+        }
+        else if("lockLog" in currentOption){ //if this option has a lock log
+            options.push(currentOption); //adds the current option to the list (assumes lock log not met)
+
+            for(let j=0; j<GameTracker.gameLogs.length; j++){
+
+                let currentLog = GameTracker.gameLogs[j]; //the current log to be checked
+
+                if(currentOption.lockLog === currentLog){ //if the lock log has been logged
+                    options.pop(currentOption); //removes the current option from the list
+                    break;
+                }
+            }
+        }
+        else{ //if no log condition
+            options.push(currentOption); //adds the current option to the list
+        }
     }
+
+    //all options have now been added to options array
+    //display all available options
+    let outputString = ``;
+    if(optionType === "number"){
+        outputString = `Enter a number: \n`;
+        for(let i=0; i < options.length; i++){
+            let currentOption = options[i];
+            outputString += `${i+1}. ${currentOption.choice}`;
+
+            if(i !== options.length - 1){
+                outputString += `\n`;
+            }
+        }
+        outputString += `${i+1}. `;
+    }
+    else{
+        outputString = `Enter a verb: `
+        for(let i=0; i < options.length; i++){
+            let currentOption = options[i];
+            outputString += `${currentOption.choice}`;
+
+            if(i !== options.length - 1){
+                outputString += `, `;
+            }
+        }
+    }
+
+    Terminal.outputMessage(outputString, optionsColor);
 
     allowInput = true; //allow input once options are displayed
 
